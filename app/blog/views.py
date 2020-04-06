acf4eb713240c008b4b0aacbc344bff072a52f5b
Pratik Temkar <pratiktemkarofficial@gmail.com>
2020-04-06 20:04:49 +0530

    Adding assets :bento:

--- app/blog/views.py
+++ app/blog/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'blog/home.html')
+
+def about(request):
+    return render(request, 'blog/about.html', {'title' : 'About'})
