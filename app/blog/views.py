e0dddce66707f7d47d57e9bd4cb61dac08af6082
Pratik Temkar <pratiktemkarofficial@gmail.com>
2020-04-06 23:17:01 +0530

    Users app created :wrench:

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Post
 
-# Create your views here.
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': Post.objects.all()
+    }
+    return render(request, 'blog/home.html', context)
 
 def about(request):
     return render(request, 'blog/about.html', {'title' : 'About'})
