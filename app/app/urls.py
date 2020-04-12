c47d4f3edf85a5bfe4bba93430e8ac438c5526be
Pratik Temkar <pratiktemkarofficial@gmail.com>
2020-04-12 18:18:14 +0530

    Profile added.

--- app/app/urls.py
+++ app/app/urls.py
@@ -21,6 +21,7 @@ from users import views as users_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('register/', users_views.register, name="users-register"),
+    path('profile/', users_views.profile, name="users-profile"),
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name="users-login"),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name="users-logout"),
     path('', include('blog.urls')),
--- app/users/views.py
+++ app/users/views.py
@@ -8,9 +8,11 @@ def register(request):
         form = UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
-            username = form.cleaned_data.get('username')
             messages.success(request, f'Your account has been created!')
             return redirect('users-login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form' : form, 'title' : 'Register'})
+
+def profile(request):
+    return render(request, 'users/profile.html')
