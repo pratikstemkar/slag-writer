bd622a8362b638079ea5e3e95fc52bf152142898
Pratik Temkar <pratiktemkarofficial@gmail.com>
2020-04-12 18:01:02 +0530

    Log In Working :wrench:

--- app/users/views.py
+++ app/users/views.py
@@ -9,11 +9,8 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f'Account created for {username}!')
-            return redirect('blog-home')
+            messages.success(request, f'Your account has been created!')
+            return redirect('users-login')
     else:
         form = UserRegisterForm()
     return render(request, 'users/register.html', {'form' : form, 'title' : 'Register'})
-
-def login(request):
-    return render(request, 'users/login.html', {'title': 'Login'})
