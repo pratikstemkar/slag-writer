6672d5f541717f6dc6a936381b49affe055f1fa1
Pratik Temkar <pratiktemkarofficial@gmail.com>
2020-04-14 13:03:03 +0530

    Pagination added.

--- app/app/urls.py
+++ app/app/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('profile/', users_views.profile, name="users-profile"),
     path('login/', auth_views.LoginView.as_view(template_name = 'users/login.html'), name="users-login"),
     path('logout/', auth_views.LogoutView.as_view(template_name = 'users/logout.html'), name="users-logout"),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name = 'users/password_reset.html'), name="password_reset"),
     path('', include('blog.urls')),
 ]
 
